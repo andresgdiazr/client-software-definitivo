e6acede30480a08c9c940d793b34c868cf05b0f1
Cristian Rosales <alejandro7120@gmail.com>
2024-06-19 20:03:27 -0400

    add longin page

--- src/App.js
+++ src/App.js
@@ -5,12 +5,13 @@ import './index.css'
 import HomeCliente from './rutas/Cliente/Home/HomeCliente';
 import Movimientos from './rutas/Cliente/Movimientos/Movimientos';
 import Home from './rutas/fintech/home';
+import Login from './rutas/fintech/login';
 
 function App() {
 
   return (
     <div>
-      <Home />
+      <Login />
     </div>
   );
 }
