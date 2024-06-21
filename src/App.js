eb4f82dbe6154568b22cf3d31aaafc81088fdab9
andresgdr <30androz@gmail.com>
2024-06-21 11:45:39 -0400

    tablas arreglado

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { RouterProvider } from 'react-router-dom';
 import router from './rutas';
 import './index.css'
 import HomeCliente from './rutas/Cliente/Home/HomeCliente';
-import Movimientos from './rutas/Cliente/Movimientos/Movimientos';
 
 function App() {
 
